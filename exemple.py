93918d9221a2329c4a5f124d91af9a2454f7a6fb
Williams CCC <47005912+CCCWilliams@users.noreply.github.com>
2019-06-04 19:06:48 +0200

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -10,6 +10,17 @@ appImage = "power.ppm" #Dimension 600x250
 appCouleur = "grey"
 appFontCouleur = "white"
 
+#On cree la liste des membres de la famille
+ma_famille = {
+'Patrice':'46 ans\nAdore les gratins de pate et le velo\nDeteste les mouches',
+'Carine':'42 ans\nAdore le vin\nDeteste le froid',
+'Williams':'23 ans\nAdore les bonbons et l\'informatique\nDeteste perdre des objets',
+'Ryan':'22 ans\nAdore les blagues\nDeteste les oignons',
+'Chloe':'17ans\nAdore l\'athetisme\nDeteste les sodas',
+'Carla':'14ans\nAdore les fast food\nDeteste les insectes'
+}
+
+
 #On cree la page
 fenetre = Tk()
 
@@ -52,16 +63,6 @@ Label(fenetre, text="Resulat :", bg=appCouleur, fg=appFontCouleur, font="none 12
 #On cree la boite pour le resultat de la recherche et on le place en dessous
 texte_resultat = Text(fenetre, height=6, wrap=WORD, background="white")
 texte_resultat.grid(row=5, column=0, columnspan=2, sticky=W)
-
-#On cree la liste des membres de la famille
-ma_famille = {
-'Patrice':'46 ans\nAdore les gratins de pate et le velo\nDeteste les mouches',
-'Carine':'42 ans\nAdore le vin\nDeteste le froid',
-'Williams':'23 ans\nAdore les bonbons et l\'informatique\nDeteste perdre des objets',
-'Ryan':'22 ans\nAdore les blagues\nDeteste les oignons',
-'Chloe':'17ans\nAdore l\'athetisme\nDeteste les sodas',
-'Carla':'14ans\nAdore les fast food\nDeteste les insectes'
-}
 
 #On cree un nouveau cadre pour quitter l'application
 Label(fenetre, text="Cliquer pour quitter :", bg=appCouleur, fg="white", font="none 12 bold").grid(row=6, column=0, sticky=W)

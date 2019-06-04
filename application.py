ed1371e70fc33bd0da2bc6178635d489ae0a36a8
Williams CCC <47005912+CCCWilliams@users.noreply.github.com>
2019-06-04 19:13:22 +0200

    Update application.py

--- application.py
+++ application.py
@@ -8,17 +8,24 @@ appImage = " ... " #Dimension 600x250
 appCouleur = " ... "
 appFontCouleur = " ... "
 
+#On cree la liste des membres de la famille
+ma_famille = {
+'...':'... \n...',
+'...':'... \n...',
+'...':'... \n...'
+}
+
 #On cree la page
 fenetre = Tk()
 
 #On prepare les fonctions que nous allons utiliser
 def recherche():
-    text_saisie=bar_saisie.get() #Je recupere ce qu'il y a dans la bar de saisie
+    texte_saisie=barre_saisie.get() #Je recupere ce qu'il y a dans la barre de saisie
     texte_resultat.delete(0.0, END)
     try:
-        membre = ma_famille[text_saisie]
+        membre = ma_famille[texte_saisie]
     except:
-        membre =  text_saisie + " ... "
+        membre =  texte_saisie + " ... "
     texte_resultat.insert(END, membre)
 
 def quitter_fenetre():
@@ -38,8 +45,8 @@ Label(fenetre, image=photo, bg=appCouleur).grid(row=0, column=0, sticky = W)
 Label(fenetre, text=" ... ", bg=appCouleur, fg=appFontCouleur, font= "none 12 bold").grid(row=0, column=0, sticky = W)
 
 #On cree la bar de saisie pour l'utilisateur et on la place en dessous
-bar_saisie= Entry(fenetre, width=20, bg="white")
-bar_saisie.grid(row=2, column=0, sticky=W)
+barre_saisie= Entry(fenetre, width=20, bg="white")
+barre_saisie.grid(row=2, column=0, sticky=W)
 
 #On ajoute un bouton
 Button(fenetre, text="Valider", width=6, command=recherche).grid(row=3, column=0, sticky=W)
@@ -50,13 +57,6 @@ Label(fenetre, text="Resulat :", bg=appCouleur, fg=appFontCouleur, font="none 12
 #On cree la boite pour le resultat de la recherche et on le place en dessous
 texte_resultat = Text(fenetre, height=6, wrap=WORD, background="white")
 texte_resultat.grid(row=5, column=0, columnspan=2, sticky=W)
-
-#On cree la liste des membres de la famille
-ma_famille = {
-'...':'... \n...',
-'...':'... \n...',
-'...':'... \n...'
-}
 
 #On cree un nouveau cadre pour quitter l'application
 Label(fenetre, text="Cliquer pour quitter :", bg=appCouleur, fg="white", font="none 12 bold").grid(row=6, column=0, sticky=W)
